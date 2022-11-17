FIX: molecules can rotate about body with default method of rotation

molgri/bodies.py
@@ -468,13 +468,12 @@ class Molecule(AbstractShape):
         for atom in self.atoms:
             atom.rotate_about_origin(angles, **kwargs)
 
-    def rotate_about_body(self, angles: np.ndarray or float, **kwargs):
-        super().rotate_about_body(angles, **kwargs)
+    def rotate_about_body(self, angles: np.ndarray or float, method="euler_123", **kwargs):
+        super().rotate_about_body(angles, method=method, **kwargs)
         inverse = kwargs.pop("inverse", False)
         for atom in self.atoms:
             points_at_origin = atom.position - self.position
-            # TODO: what is happening here?
-            rot = atom._rotate(angles, **kwargs)
+            rot = atom._rotate(angles, method=method, **kwargs)
             atom.position = rot.apply(points_at_origin, inverse=inverse) + self.position
 
 

tests/test_bodies.py
@@ -163,7 +163,7 @@ def test_everything_runs():
         atomic_obj.draw(ax, show_basis=True)
         atomic_obj.rotate_about_body(np.array([pi / 4, pi / 6, pi / 3]))
         atomic_obj.draw(ax, show_basis=False)
-        plt.show()
+        plt.close()
 
 
 if __name__ == '__main__':