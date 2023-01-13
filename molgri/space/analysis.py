FIX: randomizing signs of quaternions gives better convergence/violin plot results

molgri/space/analysis.py
@@ -2,51 +2,11 @@ from typing import Tuple
 
 import numpy as np
 import pandas as pd
-from numpy._typing import NDArray
 from numpy.typing import NDArray
 from scipy.constants import pi
 
-from molgri.space.utils import angle_between_vectors
-
-
-def random_sphere_points(n: int = 1000) -> NDArray:
-    """
-    Create n points that are truly randomly distributed across the sphere. Eg. to test the uniformity of your grid.
-
-    Args:
-        n: number of points
-
-    Returns:
-        an array of grid points, shape (n, 3)
-    """
-    phi = np.random.uniform(0, 2*pi, (n, 1))
-    costheta = np.random.uniform(-1, 1, (n, 1))
-    theta = np.arccos(costheta)
-
-    x = np.sin(theta) * np.cos(phi)
-    y = np.sin(theta) * np.sin(phi)
-    z = np.cos(theta)
-    return np.concatenate((x, y, z), axis=1)
-
-
-def random_quaternions(n: int = 1000) -> NDArray:
-    """
-    Create n random quaternions
-
-    Args:
-        n: number of points
-
-    Returns:
-        an array of grid points, shape (n, 4)
-    """
-    result = np.zeros((n, 4))
-    random_num = np.random.random((n, 3))
-    result[:, 0] = np.sqrt(1 - random_num[:, 0]) * np.sin(2 * pi * random_num[:, 1])
-    result[:, 1] = np.sqrt(1 - random_num[:, 0]) * np.cos(2 * pi * random_num[:, 1])
-    result[:, 2] = np.sqrt(random_num[:, 0]) * np.sin(2 * pi * random_num[:, 2])
-    result[:, 3] = np.sqrt(random_num[:, 0]) * np.cos(2 * pi * random_num[:, 2])
-    assert result.shape[1] == 4
-    return result
+from molgri.space.utils import angle_between_vectors, random_sphere_points, random_quaternions, \
+    standardise_quaternion_set, randomise_quaternion_set_signs
 
 
 def vector_within_alpha(central_vec: NDArray, side_vector: NDArray, alpha: float) -> bool or NDArray:
@@ -83,7 +43,9 @@ def random_axes_count_points(array_points: np.ndarray, alpha: float, num_random_
 
 def random_quaternions_count_points(array_points: np.ndarray, alpha: float, num_random_points: int = 1000):
     central_vectors = random_quaternions(num_random_points)
-    # # use half-sphere in both examples - if 1st component negative, flip the whole vector
+    # # use half-sphere in both examples
+    central_vectors = randomise_quaternion_set_signs(central_vectors)
+    array_points = randomise_quaternion_set_signs(array_points)
     # for i, line in enumerate(central_vectors):
     #     if line[0] < 0:
     #         central_vectors[i, :] = -central_vectors[i, :]

molgri/space/rotobj.py
@@ -6,7 +6,8 @@ from scipy.constants import pi
 from scipy.spatial.distance import cdist
 from scipy.spatial.transform import Rotation
 
-from molgri.space.analysis import random_quaternions, prepare_statistics, write_statistics
+from molgri.space.analysis import prepare_statistics, write_statistics
+from molgri.space.utils import random_quaternions
 from molgri.constants import UNIQUE_TOL, EXTENSION_GRID_FILES, GRID_ALGORITHMS, NAME2PRETTY_NAME
 from molgri.molecules.parsers import GridNameParser
 from molgri.paths import PATH_OUTPUT_ROTGRIDS, PATH_OUTPUT_STAT
@@ -49,7 +50,7 @@ class GridInfo:
     def save_grid(self, additional_name=""):
         if additional_name:
             additional_name = f"_{additional_name}"
-        np.save(f"{PATH_OUTPUT_ROTGRIDS}{self.standard_name}{additional_name}.{EXTENSION_GRID_FILES}", self.grid)
+        np.save(f"{PATH_OUTPUT_ROTGRIDS}{self.standard_name}{additional_name}.{EXTENSION_GRID_FILES}", self.get_grid())
 
     def save_grid_txt(self):
         # noinspection PyTypeChecker

tests/test_analysis.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy.constants import pi
 
-from molgri.space.utils import normalise_vectors, angle_between_vectors, dist_on_sphere, norm_per_axis
-from molgri.space.analysis import random_sphere_points, vector_within_alpha, count_points_within_alpha, \
+from molgri.space.utils import normalise_vectors, angle_between_vectors, dist_on_sphere, norm_per_axis, \
+    random_sphere_points
+from molgri.space.analysis import vector_within_alpha, count_points_within_alpha, \
     random_axes_count_points
 from molgri.space.rotobj import build_grid
 

tests/test_rotations.py
@@ -2,8 +2,7 @@ from numpy.typing import NDArray
 
 from molgri.space.rotations import Rotation2D, grid2euler, euler2grid, quaternion2grid, \
     grid2quaternion, grid2rotation, rotation2grid
-from molgri.space.utils import normalise_vectors
-from molgri.space.analysis import random_quaternions
+from molgri.space.utils import normalise_vectors, random_quaternions
 from scipy.spatial.transform import Rotation
 
 from scipy.constants import pi

tests/test_utils.py
@@ -1,7 +1,9 @@
 
 import numpy as np
 
-from molgri.space.utils import normalise_vectors
+from molgri.space.utils import normalise_vectors, standardise_quaternion_set, random_quaternions, \
+    randomise_quaternion_set_signs
+from tests.test_rotations import assert_two_sets_of_quaternions_equal
 
 
 def test_normalising():
@@ -47,3 +49,16 @@ def test_normalising():
     assert normalised6.shape == my_array6.shape
     assert np.allclose(np.linalg.norm(normalised6, axis=0), length6)
 
+
+def test_standardise_quaternion_set():
+    quaternions = random_quaternions(500)
+    base = random_quaternions(1).reshape((4,))
+    standard_quat = standardise_quaternion_set(quaternions, base)
+    assert_two_sets_of_quaternions_equal(quaternions, standard_quat)
+    assert np.all(base.dot(standard_quat.T)) >= 0
+
+
+def test_randomise_quaternion_set_signs():
+    quaternions = random_quaternions(500)
+    rand_quaternions = randomise_quaternion_set_signs(quaternions)
+    assert_two_sets_of_quaternions_equal(quaternions, rand_quaternions)