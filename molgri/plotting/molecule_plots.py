2787e2ef806a82daa1a2a3ce0b6f32ff724acb13
Hana Zupan <hana.zupan@hotmail.com>
2023-02-15 10:14:00 +0100

    PLOT: starting to refactor molecular plots

---
+++ molgri/plotting/molecule_plots.py
@@ -0,0 +1,13 @@
+from molgri.plotting.abstract import RepresentationCollection
+
+
+class TrajectoryPlot(RepresentationCollection):
+
+    def __init__(self, parsed_file):
+        super().__init__(data_name)
+
+    def make_trajectory_plot(self):
+        pass
+
+    def make_energy_position_plot(self):
+        pass
