FEAT: fully functional implementation of MSM and SQRA including all typical implementation, corrected normalisation of rate matrices

molgri/molecules/transitions.py
@@ -148,7 +148,7 @@ class MSM(TransitionModel):
             # in this case always move the window by step and use all points in simulations to count transitions
             for k in range(0, len(seq) - len_window, step):
                 start_stop_list = seq[k: k + len_window + 1:len_window]
-                if np.NaN not in start_stop_list:
+                if not np.isnan(start_stop_list).any():
                     yield tuple([int(el) for el in start_stop_list])
 
         def noncorr_window(seq: Sequence, len_window: int) -> Tuple[Any, Any]:
@@ -232,18 +232,28 @@ class SQRA(TransitionModel):
         energy_counts = np.zeros(shape=(self.num_cells,))
         obtained_energies = self.sim_hist.parsed_trajectory.get_all_energies(energy_type=energy_type)
         for a, e in zip(self.assignments, obtained_energies):
-            if a != np.NaN:
+            if not np.isnan(a):
                 all_energies[int(a)] += e
                 energy_counts[int(a)] += 1
-        all_energies = np.where(energy_counts == 0, all_energies, all_energies/energy_counts)
+        # in both cases avoiding division with zero
+        all_energies = np.divide(all_energies, energy_counts, out=np.zeros_like(all_energies), where=energy_counts != 0)
+        rate_matrix = np.divide(D * all_surfaces, all_distances, out=np.zeros_like(D * all_surfaces),
+                                where=all_distances!=0)
 
-        rate_matrix = D * all_surfaces / all_distances
         for i, _ in enumerate(rate_matrix):
-            rate_matrix[i] /= (all_volumes[i]*np.sqrt(all_energies[i]))
-
+            divide_by = all_volumes[i]*np.sqrt(np.abs(all_energies[i]))
+            rate_matrix[i] = np.divide(rate_matrix[i], divide_by, out=np.zeros_like(rate_matrix[i]),
+                                    where=(divide_by != 0 and divide_by != np.NaN))
         for j, _ in enumerate(rate_matrix):
-            rate_matrix[:, j] *= np.sqrt(all_energies[j])
+            multiply_with = np.sqrt(np.abs(all_energies[j]))
+            rate_matrix[:, j] = np.multiply(rate_matrix[:, j], multiply_with, out=np.zeros_like(rate_matrix[:, j]),
+                                       where=multiply_with != np.NaN)
 
+        # normalise rows
+        sums = np.sum(rate_matrix, axis=1)
+        np.fill_diagonal(rate_matrix, -sums)
+
+        # additional axis
         rate_matrix = rate_matrix[np.newaxis, :]
         assert rate_matrix.shape == (1, self.num_cells, self.num_cells)
         return rate_matrix

molgri/plotting/transition_plots.py
@@ -15,7 +15,7 @@ class TransitionPlot(RepresentationCollection):
         data_name = self.transition_obj.get_name()
         super().__init__(data_name, *args, **kwargs)
 
-    def make_its_plot(self, fig=None, ax=None, save=True, num_eigenv=6):
+    def make_its_plot(self, fig=None, ax=None, save=True, num_eigenv=6, as_line=False):
         """
         Plot iterative timescales.
         """
@@ -25,10 +25,17 @@ class TransitionPlot(RepresentationCollection):
         eigenvals = np.array(eigenvals)
         dt = self.transition_obj.sim_hist.parsed_trajectory.dt
 
-        for j in range(1, num_eigenv):
-            to_plot_abs = np.array(-tau_array * dt / np.log(np.abs(eigenvals[:, j])))
-            sns.lineplot(x=tau_array * dt, y=to_plot_abs,
-                         ax=ax, legend=False)
+        if not as_line:
+            for j in range(1, num_eigenv):
+                to_plot_abs = np.array(-tau_array * dt / np.log(np.abs(eigenvals[:, j])))
+                sns.lineplot(x=tau_array * dt, y=to_plot_abs,
+                             ax=ax, legend=False)
+        else:
+            # for SQRA plot vertical lines
+            tau_array = np.concatenate(np.array([0]), tau_array)
+            for j in range(1, len(eigenvals[:num_eigenv])):
+                absolute_its = np.array([- 1 / eigenvals[j] for _ in tau_array])
+                ax.plot(tau_array * dt, absolute_its, color="black", ls="--")
 
         self.ax.set_xlim(left=0, right=tau_array[-1] * dt)
         self.ax.set_xlabel(r"$\tau$")
@@ -141,15 +148,17 @@ if __name__ == "__main__":
     parsed_trajectory.energies = pe
 
     # the exact grid used
-    fg = FullGrid(t_grid_name="linspace(0.8, 2.5, 8)", o_grid_name="ico_512", b_grid_name="zero")
+    fg = FullGrid(t_grid_name="linspace(0.8, 2.5, 2)", o_grid_name="ico_80", b_grid_name="zero")
 
     # plotting
     sh = SimulationHistogram(parsed_trajectory, fg)
     my_sqra = SQRA(sh, use_saved=False)
 
     tp = TransitionPlot(my_sqra)
-    tp.make_its_plot()
+    tp.make_its_plot(as_line=True)
     tp.make_eigenvalues_plot()
+    for i in range(3):
+        tp.make_one_eigenvector_plot(i, animate_rot=True)
 
     tp.make_eigenvectors_plot(projection="3d")
     tp.make_eigenvectors_plot(projection="hammer")