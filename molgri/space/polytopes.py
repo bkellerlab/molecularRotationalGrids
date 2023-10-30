BUG: corrected wrong def of inverse quaternion

molgri/space/polytopes.py
@@ -34,8 +34,8 @@ from scipy.constants import pi, golden
 from scipy.sparse import coo_array
 from scipy.spatial.distance import cdist
 
-from molgri.space.utils import distance_between_quaternions, normalise_vectors, dist_on_sphere, unique_quaternion_set, \
-    which_row_is_k
+from molgri.space.utils import (distance_between_quaternions, normalise_vectors, dist_on_sphere,
+                                hemisphere_quaternion_set, which_row_is_k)
 
 
 class Polytope(ABC):
@@ -582,7 +582,7 @@ class Cube4DPolytope(Polytope):
         """
 
         projected_points = self.get_nodes(projection=True)
-        unique_projected_points = unique_quaternion_set(projected_points)
+        unique_projected_points = hemisphere_quaternion_set(projected_points)
 
         all_ci = []
         for upp in unique_projected_points:

molgri/space/rotobj.py
@@ -29,7 +29,7 @@ from scipy.spatial.transform import Rotation
 
 from molgri.space.analysis import prepare_statistics, write_statistics
 from molgri.space.utils import find_inverse_quaternion, random_quaternions, random_sphere_points, \
-    unique_quaternion_set, dist_on_sphere, which_row_is_k
+    hemisphere_quaternion_set, dist_on_sphere, which_row_is_k
 from molgri.constants import UNIQUE_TOL, EXTENSION_GRID_FILES, NAME2PRETTY_NAME, SMALL_NS
 from molgri.paths import PATH_OUTPUT_ROTGRIDS, PATH_OUTPUT_STAT
 from molgri.space.polytopes import Cube4DPolytope, IcosahedronPolytope, Cube3DPolytope, Polytope
@@ -475,7 +475,7 @@ class RandomQRotations(SphereGrid4Dim):
         np.random.seed(0)
         all_quaternions = random_quaternions(4*self.N)
         # now select those that are in the upper hemisphere
-        unique_quaternions = unique_quaternion_set(all_quaternions)[:self.N]
+        unique_quaternions = hemisphere_quaternion_set(all_quaternions)[:self.N]
         return unique_quaternions
 
 

molgri/space/utils.py
@@ -80,42 +80,75 @@ def dist_on_sphere(vector1: np.ndarray, vector2: np.ndarray) -> np.ndarray:
     return angle * norm1
 
 
-def unique_quaternion_set(quaternions: NDArray) -> NDArray:
+def hemisphere_quaternion_set(quaternions: NDArray, upper=True) -> NDArray:
     """
-    Select only the "upper half" of hyperspherical points (quaternions that may be repeating). How selection is done:
-    select a list of all quaternions that have non-negative first coordinate.
-    Among the nodes with first coordinate equal zero, select only the ones with non-negative second coordinate etc.
+    Select only the "upper half"/"bottom half" of hyperspherical points (quaternions that may be repeating).
+    How selection is done:
+    for all points select either q or -q, depending which is in the right hemisphere
 
     Args:
         quaternions: array (N, 4), each row a quaternion
+        upper: if True, select the upper hemisphere, that is, demand that the first non-zero coordinate is positive
 
     Returns:
         quaternions: array (M <= N, 4), each row a quaternion different from all other ones
     """
     # test input
-    all_row_norms_equal_k(quaternions, 1)
     is_array_with_d_dim_r_rows_c_columns(quaternions, d=2, c=4)
 
     non_repeating_quaternions = []
-    for i in range(4):
-        for projected_point in quaternions:
+    for projected_point in quaternions:
+        for i in range(4):
+            # if this if-sentence is True, the point is in the upper hemisphere
             if np.allclose(projected_point[:i], 0) and projected_point[i] > 0:
                 # the point is selected
+                if upper:
+                    non_repeating_quaternions.append(projected_point)
+                else:
+                    non_repeating_quaternions.append(find_inverse_quaternion(projected_point))
+                break
+        # if the loop didn't break, the point was not in upper hemisphere
+        else:
+            if upper:
+                non_repeating_quaternions.append(find_inverse_quaternion(projected_point))
+            else:
                 non_repeating_quaternions.append(projected_point)
+
     return np.array(non_repeating_quaternions)
 
 
 def find_inverse_quaternion(q: NDArray) -> NDArray:
+    """
+    Inverse quaternion -q = (-q0, -q1, -q2, -q3) is the quaternion that represents the same rotation as q.
+
+    Args:
+        q: a quaternion of shape (4,) whose inverse is needed
+
+    Returns:
+        another quaternion of shape (4,) with all coordinates inversed
+    """
     assert q.shape == (4,)
-    new_q = []
-    for i in range(1, 5):
-        if np.allclose(q[:i], 0):
-            new_q.append(0)
-        else:
-            new_q.append(-q[i-1])
-            new_q.extend(q[i:])
-            break
-    return np.array(new_q)
+    return -q
+
+
+def q_in_upper_hypersphere(q: NDArray) -> bool:
+    """
+    Determine whether q in the upper part of the hypersphere. This will be true if the first non-zero element of
+    the quaternion is positive.
+
+    The point (0, 0, 0, 0) is defined to be in the bottom hemisphere.
+
+    Args:
+        q: a quaternion to be tested
+
+    Returns:
+
+    """
+    assert q.shape == (4,)
+    for i in range(4):
+        if np.allclose(q[:i], 0) and q[i] > 0:
+            return True
+    return False
 
 def random_sphere_points(n: int = 1000) -> NDArray:
     """

tests/test_utils.py
@@ -2,10 +2,11 @@ import os
 import numpy as np
 
 from molgri.space.utils import (all_row_norms_equal_k, all_row_norms_similar, distance_between_quaternions,
-                                find_inverse_quaternion,
+                                find_inverse_quaternion, hemisphere_quaternion_set,
                                 is_array_with_d_dim_r_rows_c_columns, normalise_vectors,
                                 quaternion_in_array, random_quaternions,
-                                angle_between_vectors, dist_on_sphere, two_sets_of_quaternions_equal)
+                                angle_between_vectors, dist_on_sphere, two_sets_of_quaternions_equal,
+                                q_in_upper_hypersphere)
 from molgri.logfiles import find_first_free_index
 from scipy.constants import pi
 
@@ -175,26 +176,48 @@ def test_angle_between():
 
 def test_find_inverse_quaternion():
     q1 = np.array([1, 22, 3, 4])
-    i_q1 = np.array([-1, 22, 3, 4])
+    assert q_in_upper_hypersphere(q1)
+    i_q1 = np.array([-1, -22, -3, -4])
     assert np.allclose(find_inverse_quaternion(q1), i_q1)
+
     q2 = np.array([-2, 3, 3, 0])
-    i_q2 = np.array([2, 3, 3, 0])
+    assert not q_in_upper_hypersphere(q2)
+    i_q2 = np.array([2, -3, -3, 0])
     assert np.allclose(find_inverse_quaternion(q2), i_q2)
+
     q3 = np.array([0, -3, 3, -4])
-    i_q3 = np.array([0, 3, 3, -4])
+    assert not q_in_upper_hypersphere(q3)
+    i_q3 = np.array([0, 3, -3, 4])
     assert np.allclose(find_inverse_quaternion(q3), i_q3), f"{find_inverse_quaternion(q3)}!={i_q3}"
+
     q4 = np.array([0, 0, 3, -4])
-    i_q4 = np.array([0, 0, -3, -4])
+    assert q_in_upper_hypersphere(q4)
+    i_q4 = np.array([0, 0, -3, 4])
     assert np.allclose(find_inverse_quaternion(q4), i_q4), f"{find_inverse_quaternion(q4)}!={i_q4}"
+
     q5 = np.array([0, 0, 0, -4])
+    assert not q_in_upper_hypersphere(q5)
     i_q5 = np.array([0, 0, 0, 4])
     assert np.allclose(find_inverse_quaternion(q5), i_q5), f"{find_inverse_quaternion(q5)}!={i_q5}"
+
     q6 = np.array([0, 0, 2, 0])
+    assert q_in_upper_hypersphere(q6)
     i_q6 = np.array([0, 0, -2, 0])
     assert np.allclose(find_inverse_quaternion(q6), i_q6), f"{find_inverse_quaternion(q6)}!={i_q6}"
+
     q7 = np.array([0, 0, 0, 0])
+    assert not q_in_upper_hypersphere(q7)
     i_q7 = np.array([0, 0, 0, 0])
     assert np.allclose(find_inverse_quaternion(q7), i_q7), f"{find_inverse_quaternion(q7)}!={i_q7}"
+
+    # test a whole array of several quaternions
+    all_qs = np.array([[1, 22, 3, 4], [-2, 3, 3, 0], [0, -3, 3, -4], [0, 0, 3, -4], [0, 0, 0, -4], [0, 0, 2, 0], [0, 0, 0, 0]])
+    expected_up = [[1, 22, 3, 4], [2, -3, -3, 0], [0, 3, -3, 4], [0, 0, 3, -4], [0, 0, 0, 4], [0, 0, 2, 0], [0, 0, 0, 0]]
+    expected_down = [[-1, -22, -3, -4], [-2, 3, 3, 0], [0, -3, 3, -4], [0, 0, -3, 4], [0, 0, 0, -4], [0, 0, -2, 0], [0, 0, 0, 0]]
+    # upper
+    assert np.allclose(hemisphere_quaternion_set(all_qs), expected_up)
+    # bottom
+    assert np.allclose(hemisphere_quaternion_set(all_qs, upper=False), expected_down)
 
 
 def test_all_row_norms_similar():