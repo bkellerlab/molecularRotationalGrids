bb4cbe64658a800732066b76b6500d6142903ba9
Hana Zupan <hana.zupan@hotmail.com>
2024-02-08 16:36:06 +0100

    determined direction when using primary axes of inertia

--- molgri/molecules/pts.py
+++ molgri/molecules/pts.py
@@ -12,7 +12,7 @@ from scipy.spatial.transform import Rotation
 
 from molgri.molecules.parsers import ParsedMolecule
 from molgri.space.fullgrid import FullGrid
-from molgri.space.utils import normalise_vectors
+from molgri.space.utils import normalise_vectors, q_in_upper_sphere
 
 
 class Pseudotrajectory:
@@ -39,6 +39,20 @@ class Pseudotrajectory:
     def get_molecule(self):
         return self.molecule
 
+    def determine_positive_directions(self):
+        pas = self.molecule.atoms.principal_axes()
+        com = self.molecule.atoms.center_of_mass()
+        directions = [0, 0, 0]
+        for atom_pos in self.molecule.atoms.positions:
+            for i, pa in enumerate(pas):
+                cosalpha = pa.dot(atom_pos-com)/np.linalg.norm(pa-com)/np.linalg.norm(atom_pos-com)
+                #print(cosalpha)
+                directions[i] = np.sign(cosalpha)
+            if not np.any(np.isclose(directions,0)):
+                break
+        return directions
+
+
     def generate_pseudotrajectory(self) -> Generator[Tuple[int, ParsedMolecule], None, None]:
         """
         A generator of ParsedMolecule elements, for each frame one. Only deals with the molecule that moves. The
@@ -51,48 +65,50 @@ class Pseudotrajectory:
         fg = self.full_grid.get_full_grid_as_array()
         # center second molecule if not centered yet
         self.molecule.translate_to_origin()
-        self.molecule.atoms.align_principal_axis(0, [1, 0, 0])
-        #self.molecule.atoms.align_principal_axis(1, [0, 1, 0])
-        self.molecule.atoms.align_principal_axis(2, [0, 1, 0])
-        print("start", np.round(self.molecule.atoms.principal_axes().T, 3))
 
+
+        #self.molecule.atoms.align_principal_axis(0, [1, 0, 0])
+        #self.molecule.atoms.align_principal_axis(1, [0, 1, 0])
+        #self.molecule.atoms.align_principal_axis(2, [0, 0, 1])
+
+        starting_positions = self.molecule.atoms.positions
+        initial_direction = self.determine_positive_directions()
+        print(initial_direction)
+        # TODO: force starting principal axes to be np.eye?
+        print("start", np.round(self.molecule.atoms.principal_axes().T, 3))
         for se3_coo in fg:
+            self.molecule.atoms.positions = starting_positions
             position = se3_coo[:3]
             orientation = se3_coo[3:]
             rotation_body = Rotation.from_quat(orientation)
+            R = rotation_body.as_matrix()
 
             QR = self.molecule.atoms.principal_axes().T
-            R = rotation_body.as_matrix()
+            print("quat", np.round(orientation, 3))
             #print("QR", np.round(QR, 3))
-            print("R", np.round(R, 3))
+            #print("R", np.round(R, 3))
 
             self.molecule.atoms.rotate(rotation_body.as_matrix(), point=self.molecule.atoms.center_of_mass())
             self.molecule.atoms.translate(position)
 
             QC = self.molecule.atoms.principal_axes().T
-            produkt = R@QR
-            #print("QC", np.round(QC, 3))
-            #print("produkt", np.round(produkt, 3))
 
-            mom_inertia = self.molecule.atoms.moment_of_inertia()
-            eigenval, eigenvec = np.linalg.eig(mom_inertia)
-            #print(eigenval)
-            print(np.round(eigenvec, 3))
-
-            from MDAnalysis.analysis import align
-
-            #print(np.round(align.rotation_matrix(QR, rotation_body.apply(QC))[0], 3))
-
-            #self.molecule.rotate_about_body(rotation_body)
-            #print("(Rc@inv(QR))", np.round((QC @ np.linalg.inv(QR)), 3))
+            direction = self.determine_positive_directions()
+            #direction = np.tile(direction, (3, 1))
+            #print("direction", np.array(direction), np.array(initial_direction))
+            #print("R@QR", direction[0]*np.round(R@QR, 3)[:, 0])
+            #print("QC", np.round(QC, 3)[:, 0])
+            #print(np.allclose(direction[2]*np.round(R@QR, 3)[:, 2], np.round(QC, 3)[:, 2]))
+            #print("missing factor", np.round(QC.T @ np.linalg.inv(R@QR).T, 3))
+            #print("R", rotation_body.as_matrix())
+            produkt = np.multiply(QC, np.tile(direction, (3, 1))) @ np.linalg.inv(QR)
+            calc_quat = np.round(Rotation.from_matrix(produkt).as_quat(), 3)
+            if not q_in_upper_sphere(calc_quat):
+                calc_quat = -calc_quat
+            print("end", calc_quat)
+            print(np.allclose(orientation, calc_quat, atol=1e-3, rtol=1e-3))
             yield self.current_frame, self.molecule
             self.current_frame += 1
-            # rotate back
-            rotation_body_i = rotation_body.inv()
-
-            self.molecule.atoms.translate(-position)
-            self.molecule.atoms.rotate(rotation_body_i.as_matrix(), point=self.molecule.atoms.center_of_mass())
-            #self.molecule.rotate_about_body(rotation_body, inverse=True)
 
 
 
