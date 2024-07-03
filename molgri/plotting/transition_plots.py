1385c1e279ce027b455cc07a9cd910199a23975a
Hana Zupan <hana.zupan@hotmail.com>
2024-07-03 10:49:28 +0200

    updated all sqra-relevant plots to plotly

--- molgri/plotting/transition_plots.py
+++ molgri/plotting/transition_plots.py
@@ -7,6 +7,8 @@ A collection of methods to visualise the SqRA or MSM objects.
 import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
+import plotly.express as px
+from scipy import sparse
 
 pio.templates.default = "simple_white"
 
@@ -60,16 +62,14 @@ def plot_its_as_line(input_data_path: str, output_data_path: str) -> None:
     fig.write_image(output_data_path, width=600, height=600)
 
 
-def plot_one_eigenvector_flat(input_data_path: str, output_data_path: str, eigenvec_index: int = 1, index_tau=0):
+def plot_one_eigenvector_flat(input_data_path: str, output_data_path: str, eigenvec_index: int = 1, index_tau: int = 0):
     """
     
     Args:
         input_data_path (): path to a .npy file that contains eigenvectors
-        output_data_path (): 
-        eigenvec_index (): 
-        index_tau (): 
-
-    Returns:
+        output_data_path (): path to the output file where the figure will be saved
+        eigenvec_index (): which eigenvector to plot
+        index_tau (): which tau index to use for plotting
 
     """
     eigenvecs = np.load(input_data_path)
@@ -81,11 +81,34 @@ def plot_one_eigenvector_flat(input_data_path: str, output_data_path: str, eigen
 
     data = eigenvecs.T[eigenvec_index]
 
-
     fig = go.Figure()
     fig.add_scatter(y=data)
     fig.update_layout(title=f"Eigenvector {eigenvec_index}", yaxis=dict(range=[np.min(data), np.max(data)]))
     fig.write_image(output_data_path, width=600, height=600)
+
+
+def plot_heatmap(input_data_path: str, output_data_path: str, index_tau: int = 0):
+    """
+    This method draws the array and colors the fields according to their values (red = very large,
+    blue = very small). Zero values are always white, negative ones always blue, positive ones always red.
+
+    Note that this may cause memory issues for very large matrices.
+
+    Args:
+        input_data_path (): path to a .npz file that contains the (sparse) rate matrix
+        output_data_path (): path to the output file where the figure will be saved
+        index_tau (): for MSM transition matrices that are a 3D numpy arrays, which tau index to use for plotting
+    """
+    transition_matrix = sparse.load_npz(input_data_path)
+
+    if len(transition_matrix.shape) == 3:
+        transition_matrix = transition_matrix[index_tau]
+
+    fig = px.imshow(transition_matrix.todense(), color_continuous_scale="RdBu_r", color_continuous_midpoint=0)
+    # cut-off because the values are over a range of magnitudes
+    fig.update_coloraxes(cmin=-5, cmid=0, cmax=5)
+    fig.write_image(output_data_path, width=600, height=600)
+
 
 def plot_its_msm(input_data_path: str, output_data_path: str) -> None:
     fig = go.Figure()
@@ -95,74 +118,10 @@ def plot_its_msm(input_data_path: str, output_data_path: str) -> None:
     pass
 
 
-
-# class TransitionPlot(RepresentationCollection):
-#
-#     def __init__(self, transition_obj: (SimulationHistogram, TransitionModel), tau_array=None, *args, **kwargs):
-#         self.simulation_histogram = transition_obj[0]
-#         self.transition_obj = transition_obj[1]
-#         self.simulation_histogram.use_saved = True
-#         self.transition_obj.use_saved = True
-#         data_name = self.transition_obj.get_name()
-#         super().__init__(data_name, *args, **kwargs)
-#
-#     @plot_method
-#     def plot_heatmap(self, trans_index: int = 0):
-#         """
-#         This method draws the array and colors the fields according to their values (red = very large,
-#         blue = very small). Zero values are always white, negative ones always blue, positive ones always red.
-#         """
-#         transition_matrix = self.transition_obj.get_transitions_matrix()[trans_index]
-#         if np.all(transition_matrix< 0):
-#             cmap = "Blues"
-#             norm = None
-#         elif np.all(transition_matrix > 0):
-#             cmap = "Reds"
-#             norm = None
-#         else:
-#             cmap = "bwr"
-#             norm = colors.TwoSlopeNorm(vcenter=0, vmax=5, vmin=-5)
-#         sns.heatmap(transition_matrix, cmap=cmap, ax=self.ax, xticklabels=False, yticklabels=False, norm=norm)
-#         self._equalize_axes()
-
-#
-#     def plot_eigenvectors(self, num_eigenvectors: int = 5, projection="3d"):
-#         """
-#         Visualize the energy surface and the first num (default=5) eigenvectors
-#         """
-#         self._create_fig_ax(num_columns=num_eigenvectors, projection=projection)
-#         for i, subax in enumerate(self.ax.ravel()):
-#             self.plot_one_eigenvector(i, ax=subax, fig=self.fig, projection=projection, save=False)
-#
-#     @plot3D_method
-#     def plot_one_eigenvector(self, eigenvec_index: int = 1):
-#         eigenvals, eigenvecs = self.transition_obj.get_eigenval_eigenvec()
-#
-#         # shape: (number_taus, number_cells, num_eigenvectors)
-#         eigenvecs = eigenvecs[0]  # values for the first tau
-#         eigenvecs = eigenvecs.T
-#
-#         fgp = FullGridPlot(self.simulation_histogram.full_grid, default_complexity_level="half_empty")
-#         fgp.plot_position_voronoi(ax=self.ax, fig=self.fig, plot_vertex_points=False, save=False)
-#         fgp.plot_positions(ax=self.ax, fig=self.fig, save=False, animate_rot=False) #, c=eigenvecs[eigenvec_index]
-#         self.ax.set_title(f"Eigenv. {eigenvec_index}")
-#
-#     @plot_method
-#     def plot_one_eigenvector_flat(self, eigenvec_index: int = 1, index_tau=0):
-#         eigenvals, eigenvecs = self.transition_obj.get_eigenval_eigenvec()
-#
-#         # shape: (number_taus, number_cells, num_eigenvectors)
-#         try:
-#             eigenvecs = eigenvecs[index_tau]  # values for the first tau
-#         except IndexError:
-#             eigenvecs = eigenvecs
-#         sns.lineplot(eigenvecs.T[eigenvec_index], ax=self.ax)
-#         self.ax.set_title(f"Eigenv. {eigenvec_index}")
-
-
 if __name__ == "__main__":
-    plot_one_eigenvector_flat("output/data/autosave/water_water-bigger_ideal_eigenvectors.npy",
-                              "output/figures/test_eval3.png")
+    plot_heatmap("output/data/autosave/water_water-small_ideal_rate_matrix.npz", "output/figures/test_eval4.png")
+    #plot_one_eigenvector_flat("output/data/autosave/water_water-bigger_ideal_eigenvectors.npy",
+    #                          "output/figures/test_eval3.png")
     #plot_its_as_line("output/data/autosave/water_water-bigger_ideal_eigenvalues.npy", "output/figures/test_eval2.png")
     #plot_eigenvalues("output/data/autosave/water_water-bigger_ideal_eigenvalues.npy", "output/figures/test_eval.png")
 
