7302b17b679c4dc42a8fcf0abb9f905ef094d8c3
Hana Zupan <hana.zupan@hotmail.com>
2024-10-17 16:28:17 +0200

    updated test_parsers for the new io setup

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from molgri.molecules.parsers import FileParser
+from molgri.io import OneMoleculeReader
 from molgri.space.translations import TranslationParser
 from molgri.naming import GridNameParser
 from molgri.constants import ANGSTROM2NM, NM2ANGSTROM, DEFAULT_ALGORITHM_O
@@ -37,34 +37,34 @@ def test_grid_name_parser():
 
 def test_atom_gro_file():
     file_name = f"molgri/examples/NA.gro"
-    my_parser = FileParser(file_name).as_parsed_molecule()
-    assert my_parser.num_atoms == 1
-    assert np.allclose(my_parser.box[:3]*ANGSTROM2NM, [30, 30, 30])
+    my_parser = OneMoleculeReader(file_name).get_molecule()
+    assert len(my_parser.atoms) == 1
+    assert np.allclose(my_parser.dimensions[:3]*ANGSTROM2NM, [30, 30, 30])
     my_molecule_set = my_parser
     my_molecule = my_molecule_set
-    assert np.allclose(my_molecule.get_center_of_mass(), [0, 0, 0])
-    assert my_molecule.atom_labels[0] == "NA"
+    assert np.allclose(my_molecule.atoms.center_of_mass(), [0, 0, 0])
+    assert my_molecule.atoms.names[0] == "NA"
     assert np.isclose(my_molecule.atoms.masses, [22.98977])
 
 
 def test_water_gro_file():
     file_name = f"molgri/examples/H2O.gro"
-    my_parser = FileParser(file_name).as_parsed_molecule()
-    assert my_parser.num_atoms == 3
-    assert np.allclose(my_parser.box[:3]*ANGSTROM2NM, [30, 30, 30])
+    my_parser = OneMoleculeReader(file_name, center_com=False).get_molecule()
+    assert len(my_parser.atoms) == 3
+    assert np.allclose(my_parser.dimensions[:3]*ANGSTROM2NM, [30, 30, 30])
     my_molecule = my_parser
     # the atomic get_positions()
     assert np.allclose(my_molecule.atoms[0].position*ANGSTROM2NM, [0.000, -0.005, 0.004])
     assert np.allclose(my_molecule.atoms[1].position*ANGSTROM2NM, [0.000,  -0.005,  -0.092])
     assert np.allclose(my_molecule.atoms[2].position*ANGSTROM2NM, [0.000,   0.087,   0.028])
     # test elements
-    assert my_molecule.atom_types[0] == "O"
-    assert my_molecule.atom_types[1] == "H"
-    assert my_molecule.atom_types[2] == "H"
+    assert my_molecule.atoms.types[0] == "O"
+    assert my_molecule.atoms.types[1] == "H"
+    assert my_molecule.atoms.types[2] == "H"
     # test gro labels
-    assert my_molecule.atom_labels[0] == "OW"
-    assert my_molecule.atom_labels[1] == "HW1"
-    assert my_molecule.atom_labels[2] == "HW2"
+    assert my_molecule.atoms.names[0] == "OW"
+    assert my_molecule.atoms.names[1] == "HW1"
+    assert my_molecule.atoms.names[2] == "HW2"
 
 
 def test_protein_gro_file():
@@ -73,57 +73,55 @@ def test_protein_gro_file():
     first time step, ignore all subsequent ones.
     """
     file_name = f"molgri/examples/example_protein.gro"
-    my_parser = FileParser(file_name).as_parsed_molecule()
-    assert my_parser.num_atoms == 902
-    assert np.allclose(my_parser.box[:3]*ANGSTROM2NM, [6.38830,  6.16418,   8.18519])
+    my_parser = OneMoleculeReader(file_name, center_com=False).get_molecule()
+    assert len(my_parser.atoms) == 902
+    assert np.allclose(my_parser.dimensions[:3]*ANGSTROM2NM, [6.38830,  6.16418,   8.18519])
     my_molecule = my_parser
     assert np.allclose(my_molecule.atoms[0].position*ANGSTROM2NM, [-0.421,  -0.191,  -1.942])
     assert np.allclose(my_molecule.atoms[1].position*ANGSTROM2NM, [-0.450,  -0.287,  -1.946])
     assert np.allclose(my_molecule.atoms[901].position*ANGSTROM2NM, [0.065,  -0.214,   2.135])
-    assert my_molecule.atom_labels[0] == "N"
-    assert my_molecule.atom_types[0] == "N"
-    assert my_molecule.atom_labels[1] == "H1"
-    assert my_molecule.atom_types[1] == "H"
-    assert my_molecule.atom_labels[4] == "CA"
-    assert my_molecule.atom_types[4] == "C"
-    assert my_molecule.atom_labels[5] == "HA"
-    assert my_molecule.atom_types[5] == "H"
-    assert my_molecule.atom_labels[345] == "CA"
-    assert my_molecule.atom_types[345] == "C"
-    assert my_molecule.atom_labels[901] == "OC2"
-    assert my_molecule.atom_types[901] == "O"
+    assert my_molecule.atoms.types[0] == "N"
+    assert my_molecule.atoms.names[0] == "N"
+    assert my_molecule.atoms.names[1] == "H1"
+    assert my_molecule.atoms.types[1] == "H"
+    assert my_molecule.atoms.names[4] == "CA"
+    assert my_molecule.atoms.types[4] == "C"
+    assert my_molecule.atoms.names[5] == "HA"
+    assert my_molecule.atoms.types[5] == "H"
+    assert my_molecule.atoms.names[345] == "CA"
+    assert my_molecule.atoms.types[345] == "C"
+    assert my_molecule.atoms.names[901] == "OC2"
+    assert my_molecule.atoms.types[901] == "O"
     organic_elements = ["N", "O", "C", "H", "S", "P", "F"]
-    for el in my_molecule.atom_types:
+    for el in my_molecule.atoms.types:
         assert el in organic_elements, f"{el} not correctly identified as organic element"
 
 
 
 def test_parsing_xyz():
     file_name = "molgri/examples/glucose.xyz"
-    my_parser = FileParser(file_name)
-    my_molecule = my_parser.as_parsed_molecule()
+    my_molecule = OneMoleculeReader(file_name, center_com=False).get_molecule()
     all_types = ["C"]*6
     all_types.extend(["O"]*6)
-    assert np.all(my_molecule.atom_types == all_types)
-    assert np.allclose(my_molecule.get_positions()[0], [35.884,  30.895,  49.120])
-    assert np.allclose(my_molecule.get_positions()[1], [36.177,  29.853,  50.124])
-    assert np.allclose(my_molecule.get_positions()[6], [34.968,  30.340,  48.234])
-    assert np.allclose(my_molecule.get_positions()[11], [39.261,  32.018,  46.920])
+    assert np.all(my_molecule.atoms.types == all_types)
+    assert np.allclose(my_molecule.atoms.positions[0], [35.884,  30.895,  49.120])
+    assert np.allclose(my_molecule.atoms.positions[1], [36.177,  29.853,  50.124])
+    assert np.allclose(my_molecule.atoms.positions[6], [34.968,  30.340,  48.234])
+    assert np.allclose(my_molecule.atoms.positions[11], [39.261,  32.018,  46.920])
 
 
 def test_parsing_pdb():
     file_name = "molgri/examples/example_pdb.pdb"
-    my_parser = FileParser(file_name)
-    my_molecule = my_parser.as_parsed_molecule()
-    assert np.all(my_molecule.atom_types[0] == "N")
-    assert np.all(my_molecule.atom_types[1] == "C")
-    assert np.all(my_molecule.atom_types[10] == "H")
-    assert np.all(my_molecule.atom_types[60] == "H")
-    assert np.allclose(my_molecule.get_positions()[0], [0.000,   0.000,   0.000])
-    assert np.allclose(my_molecule.get_positions()[1], [1.456,   0.000,   0.000])
-    assert np.allclose(my_molecule.get_positions()[6], [2.010,   1.208,  -0.746])
-    assert np.allclose(my_molecule.get_positions()[11], [3.241,  -0.000,   1.742])
-    assert np.allclose(my_molecule.get_positions()[60], [14.680,  -0.233,   5.690])
+    my_molecule = OneMoleculeReader(file_name, center_com=False).get_molecule()
+    assert np.all(my_molecule.atoms.types[0] == "N")
+    assert np.all(my_molecule.atoms.types[1] == "C")
+    assert np.all(my_molecule.atoms.types[10] == "H")
+    assert np.all(my_molecule.atoms.types[60] == "H")
+    assert np.allclose(my_molecule.atoms.positions[0], [0.000,   0.000,   0.000])
+    assert np.allclose(my_molecule.atoms.positions[1], [1.456,   0.000,   0.000])
+    assert np.allclose(my_molecule.atoms.positions[6], [2.010,   1.208,  -0.746])
+    assert np.allclose(my_molecule.atoms.positions[11], [3.241,  -0.000,   1.742])
+    assert np.allclose(my_molecule.atoms.positions[60], [14.680,  -0.233,   5.690])
 
 
 def test_trans_parser():
