7f91089ae60e8cefd225e753b3a7e6e6cc831506
Hana Zupan <hana.zupan@hotmail.com>
2024-10-30 11:01:53 +0100

    playing with readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -54,5 +54,5 @@ autoclass_content = 'both'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+#html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
